5f1cf83fd59bd457de0e559417e596630a83280c
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-02-07 15:21:02 -0500

    Working int taker

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        
+        int userNumber = myScanner.nextInt();
+        int sum = 0;
+        for(int i = 1; i <= userNumber; i++) {
+        sum += i;
+        }
+        
+        System.out.println(sum);
+        
+        
+        
+        
     }
 }
